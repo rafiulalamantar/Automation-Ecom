6859d47cdb4c321e7401fee985ad6e0833d246ba
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2024-03-28 22:36:46 +0600

    implement parallel executions

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -2,8 +2,15 @@ package cucumberOptions;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true)
 //dryRun = true only use for compile
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
