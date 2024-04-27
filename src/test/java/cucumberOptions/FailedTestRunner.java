688205276a7b5b38f236906289d5441970f24c2f
Rafiul Alam Antar <53433694+rafiulalamantar@users.noreply.github.com>
2024-04-27 13:33:05 +0600

    capture failed scenario and integrate failed runner for further execution

--- src/test/java/cucumberOptions/FailedTestRunner.java
+++ src/test/java/cucumberOptions/FailedTestRunner.java
@@ -4,11 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/java/features",glue = "stepDefinitions",monochrome = true,tags = "@PlaceOrder or @OffersPage",
+@CucumberOptions(features = "@target/failed_scenario.txt",glue = "stepDefinitions",monochrome = true,tags = "@PlaceOrder or @OffersPage",
 plugin = {"html:target/cucumber.html","json:target/cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
  "return:target/failed_scenario.txt"})
 //dryRun = true only use for compile
-public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+public class FailedTestRunner extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
     @Override
